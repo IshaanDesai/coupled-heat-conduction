19605d1de5a86084006b4bb3a57c490e3176a525
Ishaan Desai <ishaandesai@gmail.com>
2022-01-19 17:27:40 +0100

    Initial adaptive mesh refinement strategy

--- micro_sim/micro_heat_circular.py
+++ micro_sim/micro_heat_circular.py
@@ -21,7 +21,6 @@ class MicroSimulation:
 
         # Set up mesh with periodicity in both X and Y directions
         self._topo, self._geom = mesh.rectilinear([np.linspace(-0.5, 0.5, nelems)] * 2, periodic=(0, 1))
-        self._topo_ref = self._topo  # Refined topology accroding to adaptive mesh refinement
 
         self._ns = function.Namespace()
         self._ns.x = self._geom
@@ -30,7 +29,7 @@ class MicroSimulation:
 
         # Physical constants
         self._ns.lam = 0.08
-        assert (self._lam > (1 / nelems)), "Diffuse interface width should be more than three elements"
+        #assert (self._lam > (1 / nelems)), "Diffuse interface width should be more than three elements"
 
         self._ns.gam = 0.03
         self._ns.eqtemp = 273  # Equilibrium temperature
@@ -78,7 +77,7 @@ class MicroSimulation:
         return 1. / (1. + function.exp(-4. / self._ns.lam * (function.sqrt(x ** 2 + y ** 2) - r + 0.001)))
 
     def vtk_output(self):
-        bezier = self._topo_ref.sample('bezier', 2)
+        bezier = self._topo.sample('bezier', 2)
         x, u, phi = bezier.eval(['x_i', 'u_i', 'phi'] @ self._ns, solu=self._solu, solphi=self._solphi)
         with treelog.add(treelog.DataLog()):
             export.vtk('micro-heat', bezier.tri, x, T=u, phi=phi)
@@ -90,19 +89,24 @@ class MicroSimulation:
         self._solphinm1 = self._solphi_checkpoint
 
     def refine_mesh(self):
-        self._topo_ref = self._topo
-        # dist = abs(self._r - function.norm2(self._geom))
-        # for margin in self._r / 2, self._r / 4, self._r / 8:
-        #     # refine elements within `margin` of the circle boundary
-        #     active, ielem = self._topo_ref.sample('bezier', 2).eval([margin - dist, self._topo_ref.f_index])
-        #     self._topo_ref = self._topo_ref.refined_by(np.unique(ielem[active > 0]))
+        print("Entering refine mesh")
+        bezier = self._topo.sample('bezier', 2)
+        phi = bezier.eval(['phi'] @ self._ns, solphi=self._solphi)
+
+        for level in range(4):
+            print("level = {}".format(level))
+            smpl = self._topo.sample('uniform', 5)
+            ielem, criterion = smpl.eval([self._topo.f_index, abs(self._ns.phi - .5) < .3], solphi=self._solphi)
+
+            # Refine the elements for which at least one point tests true.
+            self._topo = self._topo.refined_by(np.unique(ielem[criterion]))
 
     def solve_allen_cahn(self, temperature, dt):
         """
         Solving the Allen-Cahn Equation using a Newton solver.
         Returns upscaled ratio of grain and surrounding material
         """
-        resphi = self._topo_ref.integral(
+        resphi = self._topo.integral(
             '(lam^2 phibasis_n dphidt + gam phibasis_n ddwpdphi + gam lam^2 phibasis_n,i phi_,i + '
             '4 lam reacrate phibasis_n phi (1 - phi)) d:x' @ self._ns, degree=2)
 
@@ -113,20 +117,20 @@ class MicroSimulation:
         self._solphinm1 = self._solphi
 
         # Calculating ratio of grain amount for upscaling
-        psi = self._topo_ref.integral('phi d:x' @ self._ns, degree=2).eval(solphi=self._solphi)
+        psi = self._topo.integral('phi d:x' @ self._ns, degree=2).eval(solphi=self._solphi)
         print("Upscaled relative amount of sand material = {}".format(psi))
 
         return psi
 
     def solve_heat_cell_problem(self):
-        res = self._topo_ref.integral('((phi ks + (1 - phi) kg) u_i,j ubasis_ni,j + '
-                                      'ubasis_ni,j (phi ks + (1 - phi) kg) $_ij) d:x' @ self._ns, degree=4)
+        res = self._topo.integral('((phi ks + (1 - phi) kg) u_i,j ubasis_ni,j + '
+                                  'ubasis_ni,j (phi ks + (1 - phi) kg) $_ij) d:x' @ self._ns, degree=4)
 
         args = dict(solphi=self._solphi)
         self._solu = solver.solve_linear('solu', res, constrain=self._ucons, arguments=args)
 
         # Calculating effective conductivity for upscaling
-        b = self._topo_ref.integral(self._ns.eval_ij('(phi ks + (1 - phi) kg) ($_ij + du_ij) d:x'), degree=4).eval(
+        b = self._topo.integral(self._ns.eval_ij('(phi ks + (1 - phi) kg) ($_ij + du_ij) d:x'), degree=4).eval(
             solu=self._solu, solphi=self._solphi)
 
         print("Upscaled conductivity = {}".format(b.export("dense")))
@@ -141,9 +145,10 @@ def main():
     micro_problem.vtk_output()
 
     temp_values = np.arange(273.0, 350.0, 1.0)
-    t = 0
+    t = 0.0
 
     for temperature in temp_values:
+        micro_problem.refine_mesh()
         micro_problem.solve_allen_cahn(temperature, dt)
         micro_problem.solve_heat_cell_problem()
         micro_problem.vtk_output()
